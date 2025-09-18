405b414f80842a411c64da986440f911f0580ece
Lazy-CSharp-Coder <harald.nydal@gmail.com>
2025-09-18 18:36:37 +0200

    test

--- script.js
+++ script.js
@@ -15,9 +15,22 @@ function hamburgerToggle() {
 
 // Gjør funksjonen tilgjengelig i globalt scope slik at den kan brukes i inline onclick-egenskaper.
 //globalThis.hamburgerToggle = hamburgerToggle;
+darkMode = true;
 
-function darkLightModeToggle() {
+function darkLightModeToggle() 
+{
   console.log("hi from darkLight mode");
-
   
+  const mainElement = document.querySelector(".mainClass");
+  if(darkMode)
+  {
+    mainElement.classList.add("lightMode")
+    darkMode = false;
+  }
+  else
+  {
+    mainElement.classList.remove("lightMode");
+    darkMode = true;
+  }
+
 }
